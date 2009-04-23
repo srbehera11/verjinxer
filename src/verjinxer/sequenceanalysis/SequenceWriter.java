b0983a7c89947512bea56a73063223eb08f702c8
markus.kemmerling <markus.kemmerling@uni-dortmund.de>
2009-04-23 11:56:46 +0000

    Sequence has more responsibilities. Get Projectname and automatic loads the needed Files. So subclasses for reading and writing cause of different underlying datastructures.

---
+++ src/verjinxer/sequenceanalysis/SequenceWriter.java
@@ -0,0 +1,7 @@
+package verjinxer.sequenceanalysis;
+
+public class SequenceWriter extends Sequence{
+   SequenceWriter(final String projectname, Mode mode){
+      super(projectname, mode);
+   }
+}
