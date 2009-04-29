05b0d40f11f2b2e260d8b7aab459840625d37e25
markus.kemmerling@uni-dortmund.de <markus.kemmerling@uni-dortmund.de@94ea493a-7c3b-0410-831d-612bdd61bfce>
2009-04-29 17:00:34 +0000

    Some formating (code and output of QGramIndexCompressionAnalyzer)and rearrangement (method log2 goes from QGramIndexCompressionAnalyzer to MathUtils). Bug (wrong needed number of bits for a step) is fixed.

--- trunk/src/verjinxer/QGramIndexCompressionAnalyzer.java
+++ trunk/src/verjinxer/QGramIndexCompressionAnalyzer.java
@@ -1,38 +1,42 @@
 package verjinxer;
 
+import java.text.DecimalFormat;
+
 import com.spinn3r.log5j.Logger;
 
 import verjinxer.sequenceanalysis.QGramIndex;
+import verjinxer.util.MathUtils;
 
 /**
  * @author Markus Kemmerling
  */
 public class QGramIndexCompressionAnalyzer {
    private static final Logger log = Globals.log;
-	
-	private QGramIndex qgramindex;
-	private int minStepSize = Integer.MAX_VALUE;
-	private int maxStepSize = 0;
-	private long stepSizeSum = 0;
-	private int numberSteps = 0;
-	private long usedBytesNormal = 0;
-	private long usedBytesCompressed = 0;
-	private int numberPositionsCompressionPossible = 0; //counts, where compression method can be used
-	private int numberPositionsCompressionImpossible = 0; //counts, where compression method can't be used
-	
 
-	public QGramIndexCompressionAnalyzer(QGramIndex qgramindex) {
-		super();
-		this.qgramindex = qgramindex;
-	}
+   private QGramIndex qgramindex;
+   private int minStepSize = Integer.MAX_VALUE;
+   private int maxStepSize = 0;
+   private long stepSizeSum = 0;
+   private int numberSteps = 0;
+   private long usedBytesNormal = 0;
+   private long usedBytesCompressed = 0;
+   private int numberPositionsCompressionPossible = 0; // counts, where compression method can be
+                                                       // used
+   private int numberPositionsCompressionImpossible = 0; // counts, where compression method can't
+                                                         // be used
 
-	public void analyze() {
-		int positions[] = new int[qgramindex.getMaximumBucketSize()];
-		int stepSize;
-		for(int qcode = 0; qcode < qgramindex.getNumberOfBuckets(); qcode++){
-			qgramindex.getQGramPositions(qcode, positions);
-			
-			if (qgramindex.getBucketSize(qcode) > 0) { //if bucket is empty, nothing to do
+   public QGramIndexCompressionAnalyzer(QGramIndex qgramindex) {
+      super();
+      this.qgramindex = qgramindex;
+   }
+
+   public void analyze() {
+      int positions[] = new int[qgramindex.getMaximumBucketSize()];
+      int stepSize;
+      for (int qcode = 0; qcode < qgramindex.getNumberOfBuckets(); qcode++) {
+         qgramindex.getQGramPositions(qcode, positions);
+
+         if (qgramindex.getBucketSize(qcode) > 0) { // if bucket is empty, nothing to do
             // first position is ever stored as int:
             usedBytesNormal += 4;
             usedBytesCompressed += 4;
@@ -42,68 +46,81 @@ public class QGramIndexCompressionAnalyzer {
             for (int i = 1; i < qgramindex.getBucketSize(qcode); ++i) {
                stepSize = positions[i] - positions[i - 1];
                assert stepSize > 0;
-               if(stepSize < minStepSize) minStepSize = stepSize;
-               if(stepSize > maxStepSize) maxStepSize = stepSize;
-               
+               if (stepSize < minStepSize)
+                  minStepSize = stepSize;
+               if (stepSize > maxStepSize)
+                  maxStepSize = stepSize;
+
                stepSizeSum += stepSize;
                numberSteps++;
                assert stepSizeSum > 0;
                assert numberSteps > 0;
-               
-               //if possible, encode the step with a short in compressed storage, otherwise with a integer
-               if (stepSize > Short.MAX_VALUE){ //Short.MAX_VALUE = 2^15-1, maybe use 2^16 [unsigned short]
+
+               // if possible, encode the step with a short in compressed storage, otherwise with a
+               // integer
+               if (stepSize > Short.MAX_VALUE) {
                   usedBytesCompressed += 4;
                   numberPositionsCompressionImpossible++;
                } else {
                   usedBytesCompressed += 2;
                   numberPositionsCompressionPossible++;
                }
-               
+
                usedBytesNormal += 4;
                assert usedBytesNormal > 0;
                assert usedBytesCompressed > 0;
             }
          }
-		}
-	}
-	
-	public void printStatistic(){
-      double averageStepSize = (double)stepSizeSum / numberSteps;
-      
-      log.info("Average step size: %s", averageStepSize);
-      log.info("Average needed bits: %s", Math.ceil(log2(averageStepSize))+1 );
-      
-      log.info("Minimum step size: %s", minStepSize);
-      log.info("Minimum needed bits: %s", Math.ceil(log2(minStepSize))+1);
-      
-      log.info("Maximium step size: %s", maxStepSize);
-      log.info("Maximium needed bits: %s", Math.ceil(log2(maxStepSize))+1);
-      
-      if(usedBytesNormal < 1024)
-         log.info("Used bytes for nomale storage: %s", usedBytesNormal);
-      else if(usedBytesNormal < 1024*1024l)
-         log.info("Used KB for nomale storage: %s", (double)usedBytesNormal/1024);
-      else if(usedBytesNormal < 1024*1024*1024l)
-         log.info("Used MB for nomale storage: %s", (double)usedBytesNormal/1024/1024);
-      else if(usedBytesNormal < 1024*1024*1024*1024l)
-         log.info("Used GB for nomale storage: %s", (double)usedBytesNormal/1024/1024/1024);
-      
-      if(usedBytesCompressed < 1024)
-         log.info("Used bytes for compressed storage: %s", usedBytesCompressed);
-      else if(usedBytesCompressed < 1024*1024l)
-         log.info("Used KB for compressed storage: %s", (double)usedBytesCompressed/1024);
-      else if(usedBytesCompressed < 1024*1024*1024l)
-         log.info("Used MB for compressed storage: %s", (double)usedBytesCompressed/1024/1024);
-      else if(usedBytesCompressed < 1024*1024*1024*1024l)
-         log.info("Used GB for compressed storage: %s", (double)usedBytesCompressed/1024/1024/1024);
-      
-      log.info("Memory saving with compressed storage in Percent: %s", (double)(usedBytesNormal-usedBytesCompressed)/usedBytesNormal );
-      
-      log.info("At %s positions is compression possible.", numberPositionsCompressionPossible);
-      log.info("At %s positions is compression impossible.", numberPositionsCompressionImpossible);
-	}
-	
-	private double log2(double value){
-	   return Math.log10(value)/Math.log10(2);
-	}
+      }
+   }
+
+   public void printStatistic() {
+      double averageStepSize = (double) stepSizeSum / numberSteps;
+
+      DecimalFormat doubleFormater = new DecimalFormat("###,###,###,###,##0.0");
+      DecimalFormat integerFormater = new DecimalFormat("###,###,###,###,##0");
+
+      log.info("Average step size: %s", doubleFormater.format(averageStepSize));
+      log.info("Average needed bits: %s", Math.ceil(MathUtils.log2(averageStepSize + 1)));
+
+      log.info("Minimum step size: %s", integerFormater.format(minStepSize));
+      log.info("Minimum needed bits: %s", Math.ceil(MathUtils.log2(minStepSize + 1)));
+
+      log.info("Maximum step size: %s", integerFormater.format(maxStepSize));
+      log.info("Maximum needed bits: %s", Math.ceil(MathUtils.log2(maxStepSize + 1)));
+
+      if (usedBytesNormal < 1024)
+         log.info("Used bytes for normal storage: %s", doubleFormater.format(usedBytesNormal));
+      else if (usedBytesNormal < 1024 * 1024l)
+         log.info("Used KB for normal storage: %s",
+               doubleFormater.format((double) usedBytesNormal / 1024));
+      else if (usedBytesNormal < 1024 * 1024 * 1024l)
+         log.info("Used MB for normal storage: %s",
+               doubleFormater.format((double) usedBytesNormal / 1024 / 1024));
+      else if (usedBytesNormal < 1024 * 1024 * 1024 * 1024l)
+         log.info("Used GB for normal storage: %s",
+               doubleFormater.format((double) usedBytesNormal / 1024 / 1024 / 1024));
+
+      if (usedBytesCompressed < 1024)
+         log.info("Used bytes for compressed storage: %s",
+               doubleFormater.format(usedBytesCompressed));
+      else if (usedBytesCompressed < 1024 * 1024l)
+         log.info("Used KB for compressed storage: %s",
+               doubleFormater.format((double) usedBytesCompressed / 1024));
+      else if (usedBytesCompressed < 1024 * 1024 * 1024l)
+         log.info("Used MB for compressed storage: %s",
+               doubleFormater.format((double) usedBytesCompressed / 1024 / 1024));
+      else if (usedBytesCompressed < 1024 * 1024 * 1024 * 1024l)
+         log.info("Used GB for compressed storage: %s",
+               doubleFormater.format((double) usedBytesCompressed / 1024 / 1024 / 1024));
+
+      log.info(
+            "Memory saving with compressed storage in percent: %s",
+            doubleFormater.format(((double) (usedBytesNormal - usedBytesCompressed) / usedBytesNormal) * 100));
+
+      log.info("Compression is possible at %s positions.",
+            integerFormater.format(numberPositionsCompressionPossible));
+      log.info("Compression is impossible at %s positions.",
+            integerFormater.format(numberPositionsCompressionImpossible));
+   }
 }
