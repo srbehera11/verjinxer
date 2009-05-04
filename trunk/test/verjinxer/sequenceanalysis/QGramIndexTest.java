aa78755efed4020676018f13984dd368687c4466
markus.kemmerling@uni-dortmund.de <markus.kemmerling@uni-dortmund.de@94ea493a-7c3b-0410-831d-612bdd61bfce>
2009-05-04 15:36:38 +0000

    First simple test uses ArrayFile to create needed files. Begin to structure the class to write different tests.

--- trunk/test/verjinxer/sequenceanalysis/QGramIndexTest.java
+++ trunk/test/verjinxer/sequenceanalysis/QGramIndexTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -14,7 +15,96 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import verjinxer.util.ArrayFile;
+
+/**
+ * This test is a whiteboxtest for <code>QGramIndex<code>. It uses the class <code>ArrayFile<code>
+ * to generate a qpos and a qbck file on disk. The tested class <code>QGramIndex<code> also uses 
+ * an <code>ArrayFile<code> to read the files.
+ * In the beginning, a new directory 'testdata' is created, where the needed files are stored.
+ * If the directory already exists, no test are made.
+ * The new qpos and qbck files are generated before each test and deleted after.
+ * After all tested were made, the 'testdata' directory will also be deleted.
+ * 
+ * @author Markus Kemmerling
+ */
 public class QGramIndexTest {
+   
+   private static final String QPOS_FILENAME = "testing.qpos";
+   private static final String QBCK_FILENAME = "testing.qbck";
+   
+   private int[] qpos;
+   private int[] qbck;
+   private int[] bucketSizes;
+   
+   private File qposfile;
+   private File qbckfile;
+   private static File testdataDirectory;
+   
+   /**
+    * @throws java.lang.Exception
+    */
+   @BeforeClass
+   public static void setUpBeforeClass() throws Exception {
+      testdataDirectory = new File("testdata");
+      if (testdataDirectory.exists()) {
+         System.err.printf("The directory %s already exists. Exiting before damage some data.", testdataDirectory.getAbsolutePath());
+         System.exit(-1);
+      } else {
+         testdataDirectory.mkdir();
+         assert testdataDirectory.exists();
+         assert testdataDirectory.isDirectory();
+      }
+   }
+
+   /**
+    * @throws java.lang.Exception
+    */
+   @AfterClass
+   public static void tearDownAfterClass() throws Exception {
+      assert testdataDirectory.exists();
+      testdataDirectory.delete();
+      assert !testdataDirectory.exists();
+   }
+
+   /**
+    * @throws java.lang.Exception
+    */
+   @Before
+   public void setUp() throws Exception {
+      qposfile = new File(testdataDirectory.getAbsolutePath() + File.separator + QPOS_FILENAME);
+      assert !qposfile.exists();
+      qposfile.createNewFile();
+      assert qposfile.exists();
+      
+      qbckfile = new File(testdataDirectory.getAbsolutePath() + File.separator + QBCK_FILENAME);
+      assert !qbckfile.exists();
+      qbckfile.createNewFile();
+      assert qbckfile.exists();
+   }
+
+   /**
+    * @throws java.lang.Exception
+    */
+   @After
+   public void tearDown() throws Exception {
+      assert qposfile.exists();
+      qposfile.delete();
+      assert !qposfile.exists();
+      qposfile = null;
+      
+      assert qbckfile.exists();
+      qbckfile.delete();
+      assert !qbckfile.exists();
+      qposfile = null;
+      
+      qpos = null;
+      qbck = null;
+      bucketSizes = null;
+      
+      System.gc();
+   }
+   
 
    @Test
    public void testQGramIndex() {
@@ -22,43 +112,63 @@ public class QGramIndexTest {
       int[] qpos = { 0, 3, 5, 7, 23, 24,
                      7, 12, 15, 18, 23, 25, 27, 30, 31, 32,
                      1, 2, 4, 7, 9, 33,
-            40, 100, 111 };
-      int[] qbck = { 0, 6, 16, qpos.length };
+                     40, 100, 111 };
+      int[] qbck = { 0, 6, 6, 16, 22, qpos.length };
+      int[] bucketSizes = {6,0,10,6,3};
 
       File qposfile = null;
       File qbckfile = null;
       try {
-         qposfile = File.createTempFile("testing", ".qpos");
+         qposfile = new File("testing.qpos");
+         qbckfile = new File("testing.qbck");
          
-         FileOutputStream fos = new FileOutputStream(qposfile);
-         for (int i : qpos) {
-            fos.write((byte) (i >> 24));
-            fos.write((byte) (i >> 16));
-            fos.write((byte) (i >> 8));
-            fos.write((byte) (i));
-         }
-         qbckfile = File.createTempFile("testing", ".qbck");
-         fos = new FileOutputStream(qbckfile);
-         for (int i : qbck) {
-            fos.write((byte) (i >> 24));
-            fos.write((byte) (i >> 16));
-            fos.write((byte) (i >> 8));
-            fos.write((byte) (i));
-         }
+         ArrayFile af = new ArrayFile(qposfile.getAbsolutePath());
+         af.writeArray(qpos);
+         
+         af.setFilename(qbckfile.getAbsolutePath());
+         af.writeArray(qbck);
+         
+         
          //generate QGramIndex
          //TODO the generation never terminates
          QGramIndex index = new QGramIndex(qposfile.getAbsolutePath(), qbckfile.getAbsolutePath(), 10, 5, 1);
          assertEquals(5, index.q);
          assertEquals(10, index.maximumBucketSize);
+         assertEquals(1, index.getStride());
          
-      } catch (IOException e) {
+         assertEquals(qpos.length, index.getNumberOfPositions());
+         
+         for(int i = 0; i < bucketSizes.length; i++){
+            assertEquals(String.format("Error for qgram %s:", i) , bucketSizes[i], index.getBucketSize(i));
+         }
+         
+         assertEquals(bucketSizes.length, index.getNumberOfBuckets());
+         
+         for(int i = 0; i < bucketSizes.length; i++){
+            int[] array = new int[ bucketSizes[i] ];
+            index.getQGramPositions(i, array);
+            for(int j = 0; j < bucketSizes[i]; j++){
+               assertEquals(qpos[ j+qbck[i] ], array[j]);
+            }
+         }
+         
+      } catch (Exception e) {
          fail(e.toString());
       } finally {
          qposfile.delete();
          qbckfile.delete();
       }
-      
-      
+   }
+   
+   /**
+    * 
+    * @param index
+    * @param q
+    * @param maximumBucketSize
+    * @param stride
+    */
+   private static void testIndex(QGramIndex index, int q, int maximumBucketSize, int stride){
+      //TODO make assertEquals from testQGramIndex hier.
    }
 
 }
