d83bb04ccb0457e36f8d5b83784163710f04014b
markus.kemmerling <markus.kemmerling@uni-dortmund.de>
2009-05-05 16:02:46 +0000

    formating

--- test/verjinxer/sequenceanalysis/QGramIndexTest.java
+++ test/verjinxer/sequenceanalysis/QGramIndexTest.java
@@ -28,18 +28,18 @@ import verjinxer.util.ArrayFile;
  * @author Markus Kemmerling
  */
 public class QGramIndexTest {
-   
+
    private static final String QPOS_FILENAME = "testing.qpos";
    private static final String QBCK_FILENAME = "testing.qbck";
-   
+
    private int[] qpos;
    private int[] qbck;
    private int[] bucketSizes;
-   
+
    private File qposfile;
    private File qbckfile;
    private static File testdataDirectory;
-   
+
    /**
     * @throws java.lang.Exception
     */
@@ -47,7 +47,8 @@ public class QGramIndexTest {
    public static void setUpBeforeClass() throws Exception {
       testdataDirectory = new File("testdata");
       if (testdataDirectory.exists()) {
-         System.err.printf("The directory %s already exists. Exiting before damage some data.", testdataDirectory.getAbsolutePath());
+         System.err.printf("The directory %s already exists. Exiting before damage some data.",
+               testdataDirectory.getAbsolutePath());
          System.exit(-1);
       } else {
          testdataDirectory.mkdir();
@@ -77,7 +78,7 @@ public class QGramIndexTest {
       assert !qposfile.exists();
       qposfile.createNewFile();
       assert qposfile.exists();
-      
+
       qbckfile = new File(testdataDirectory.getAbsolutePath() + File.separator + QBCK_FILENAME);
       assert !qbckfile.exists();
       qbckfile.createNewFile();
@@ -94,104 +95,109 @@ public class QGramIndexTest {
       qposfile.delete();
       assert !qposfile.exists();
       qposfile = null;
-      
+
       assert qbckfile.exists();
       qbckfile.delete();
       assert !qbckfile.exists();
       qposfile = null;
-      
+
       qpos = null;
       qbck = null;
       bucketSizes = null;
-      
+
       System.gc();
       System.out.println("Files deleted.");
    }
-   
+
    @Test
    public void smallTest() {
-      qpos = new int[]{ 0, 3, 5, 7, 23, 24,
-                        7, 12, 15, 18, 23, 25, 27, 30, 31, 32,
-                        1, 2, 4, 7, 9, 33,
-                        40, 100, 111 };
-      qbck = new int[]{ 0, 6, 6, 16, 22, qpos.length };
-      bucketSizes = new int[]{6,0,10,6,3};
-      
+      qpos = new int[] { 0, 3, 5, 7, 23, 24, 7, 12, 15, 18, 23, 25, 27, 30, 31, 32, 1, 2, 4, 7, 9,
+            33, 40, 100, 111 };
+      qbck = new int[] { 0, 6, 6, 16, 22, qpos.length };
+      bucketSizes = new int[] { 6, 0, 10, 6, 3 };
+
       try {
-         
+
          writeArraysToDisc();
-         
+
          int q = 5;
          int maximumBucketSize = 10;
          int stride = 1;
-         
-         //generate QGramIndex
-         QGramIndex index = new QGramIndex(qposfile.getAbsolutePath(), qbckfile.getAbsolutePath(), maximumBucketSize, q, stride);
+
+         // generate QGramIndex
+         QGramIndex index = new QGramIndex(qposfile.getAbsolutePath(), qbckfile.getAbsolutePath(),
+               maximumBucketSize, q, stride);
          testIndex(index, maximumBucketSize, q, stride);
-         
+
       } catch (IOException e) {
          fail(e.toString());
       }
    }
-   
+
    /**
     * Writes qpos into qposfile and qbck into qbckfile.
     * 
     * @throws IOException
     */
-   private void writeArraysToDisc() throws IOException{
+   private void writeArraysToDisc() throws IOException {
       ArrayFile af = new ArrayFile(qposfile.getAbsolutePath());
       af.writeArray(qpos);
       af.flush();
-      
+
       af.setFilename(qbckfile.getAbsolutePath());
       af.writeArray(qbck);
       af.flush();
       af.close();
    }
-   
+
    /**
     * Test the following methods of QGramIndex:<br>
     * {@link verjinxer.sequenceanalysis.QGramIndex#getStride()}
     * {@link verjinxer.sequenceanalysis.QGramIndex#getNumberOfPositions()}
     * {@link verjinxer.sequenceanalysis.QGramIndex#getNumberOfBuckets()}
     * {@link verjinxer.sequenceanalysis.QGramIndex#getBucketSize(int)}
-    * {@link verjinxer.sequenceanalysis.QGramIndex#getQGramPositions(int, int[])}
-    * And for the following fields:
-    * {@link verjinxer.sequenceanalysis.QGramIndex#q}
+    * {@link verjinxer.sequenceanalysis.QGramIndex#getQGramPositions(int, int[])} And for the
+    * following fields: {@link verjinxer.sequenceanalysis.QGramIndex#q}
     * {@link verjinxer.sequenceanalysis.QGramIndex#maximumBucketSize}
-    * @param index QGramIndex to test
-    * @param q the expected q
-    * @param maximumBucketSize the expected size of the buckets
-    * @param stride the expected stride
+    * 
+    * @param index
+    *           QGramIndex to test
+    * @param q
+    *           the expected q
+    * @param maximumBucketSize
+    *           the expected size of the buckets
+    * @param stride
+    *           the expected stride
     */
-   private void testIndex(QGramIndex index, int maximumBucketSize, int q, int stride){
+   private void testIndex(QGramIndex index, int maximumBucketSize, int q, int stride) {
       System.out.println("Testing q.");
       assertEquals(q, index.q);
-      
+
       System.out.println("Testing maximum bucket size.");
       assertEquals(maximumBucketSize, index.maximumBucketSize);
-      
+
       System.out.println("Testing stride.");
       assertEquals(stride, index.getStride());
-      
+
       System.out.println("Testing number of positions.");
       assertEquals(qpos.length, index.getNumberOfPositions());
-      
+
       System.out.println("Testing number of buckets.");
       assertEquals(bucketSizes.length, index.getNumberOfBuckets());
-      
+
       System.out.println("Testing sizes of buckets.");
-      for(int i = 0; i < bucketSizes.length; i++){
-         assertEquals(String.format("Wrong bucket size for qgram %s:", i) , bucketSizes[i], index.getBucketSize(i));
+      for (int i = 0; i < bucketSizes.length; i++) {
+         assertEquals(String.format("Wrong bucket size for qgram %s:", i), bucketSizes[i],
+               index.getBucketSize(i));
       }
-      
-      for(int i = 0; i < bucketSizes.length; i++){
-         System.out.printf("Testing positions for qgram %s.%n",i);
-         int[] array = new int[ bucketSizes[i] ];
+
+      for (int i = 0; i < bucketSizes.length; i++) {
+         System.out.printf("Testing positions for qgram %s.%n", i);
+         int[] array = new int[bucketSizes[i]];
          index.getQGramPositions(i, array);
-         for(int j = 0; j < bucketSizes[i]; j++){
-            assertEquals(String.format("Position %s for qgram %s is wrong.", j, i), qpos[ j+qbck[i] ], array[j]);
+         for (int j = 0; j < bucketSizes[i]; j++) {
+            assertEquals(String.format("Position %s for qgram %s is wrong.", j, i), qpos[j
+                  + qbck[i]], array[j]);
          }
       }
    }
