e768db0cd552acdb860af96831ce4c9889ca4bc8
markus.kemmerling <markus.kemmerling@uni-dortmund.de>
2009-05-13 15:36:39 +0000

    New Enumeration FileTypes which replaces FileUtils.FileType and will replace FileNameExtensions.

---
+++ src/verjinxer/util/FileTypes.java
@@ -0,0 +1,41 @@
+package verjinxer.util;
+
+/**
+ * @author Markus Kemmerling
+ */
+public enum FileTypes {
+   SEQ("seq"),
+   PRJ("prj"),
+   LOG("log"),
+   DESC("desc"),
+   SELECT("select-filter"),
+   SSP("ssp"),
+   ALPHABET("alphabet"),
+   QBUCKETS("qbck"),
+   QPOSITIONS("qpos"),
+   QFREQ("qfreq"),
+   QSEQFREQ("qsfrq"),
+   RUNSEQ("runseq"),
+   RUNLEN("runlen"),
+   RUN2POS("run2pos"),
+   POS2RUN("pos2run"),
+   POS("pos"),         // suffix array
+   LCP("lcp"),         // lcp values
+   QUALITIY("quality"),
+   MAPPED("mapped"),
+   
+   FASTA("fasta"),
+   CSFASTA("csfasta"),
+   TEXT("txt");
+   
+   private final String extension;
+
+   private FileTypes(String extension) {
+      this.extension = "." + extension;
+   }
+
+   public String toString() {
+      return extension;
+   }
+   
+}
