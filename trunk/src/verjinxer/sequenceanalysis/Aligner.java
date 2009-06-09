8255e5ecb4e19e4369488b5c4416a9fe762e3c78
markus.kemmerling@uni-dortmund.de <markus.kemmerling@uni-dortmund.de@94ea493a-7c3b-0410-831d-612bdd61bfce>
2009-06-09 18:10:43 +0000

    Aligner.semiglobalAlign(byte[], byte[]) is now usable. Simple test SISSI-MISSISSIPPI works.

--- trunk/src/verjinxer/sequenceanalysis/Aligner.java
+++ trunk/src/verjinxer/sequenceanalysis/Aligner.java
@@ -20,10 +20,10 @@ public class Aligner {
       public static int MISMATCH = 1;
    }
 
-   private static final byte GAP = -1; // TODO
+   public static final byte GAP = -1; // TODO
 
    /** DP table entry */
-   private class Entry {
+   private static class Entry {
       public int score;
       public Direction backtrack;
    }
@@ -302,118 +302,106 @@ public class Aligner {
     * 
     * @param s1
     * @param s2
-    * @param m
-    * @param n
     */
-   void semiglobalAlign(byte[] s1, byte[] s2, int m, int n) {
-      // byte[] s1;
-      // byte[] s2;
-      // int m, n;
-
-      /*            s2 (n, j)
+   public static SemiglobalAlignmentResult semiglobalAlign(final byte[] s1, final byte[] s2) {
+      /*            s2 (column:1..n)
             --------------->
            |
-        s1 | (m, i)
+        s1 | (row:1..m)
            |
            V
       */
+      
+      final int m = s1.length;
+      final int n = s2.length;
 
-      // the DP table is stored column-wise
-      Entry[][] columns;
-      columns = new Entry[n + 1][m + 1];
+      // the DP table row x column
+      Entry[][] table = new Entry[m + 1][n + 1];
 
-      int i, j;
+      int row, column;
 
-      for (i = 0; i < columns.length; ++i) {
-         for (j = 0; j < columns[i].length; ++j) {
-            columns[i][j] = new Entry();
+      for (row = 0; row < table.length; ++row) {
+         for (column = 0; column < table[row].length; ++column) {
+            table[row][column] = new Entry();
          }
       }
 
-      for (i = 0; i <= m; ++i) {
-         columns[0][i].score = 0;
-         columns[0][i].backtrack = Direction.UP; // TODO never read
+      for (row = 0; row < table.length; ++row) {
+         table[row][0].score = 0;
+         table[row][0].backtrack = Direction.LEFT; // TODO never read
       }
-      for (j = 0; j <= n; ++j) {
-         columns[j][0].score = 0;
-         columns[j][0].backtrack = Direction.LEFT; // TODO never read
+      for (column = 0; column < table[0].length; ++column) {
+         table[0][column].score = 0;
+         table[0][column].backtrack = Direction.UP; // TODO never read
       }
 
       // calculate alignment (using unit costs)
-      // outer loop goes over columns
-      Entry[] cur_column;
-      Entry[] prev_column = columns[0];
-      for (j = 1; j <= n; ++j) {
-         cur_column = columns[j];
-         for (i = 1; i <= m; ++i) {
-            Direction bt = Direction.DIAG;
-            int score = prev_column[i - 1].score + ((s1[i - 1] == s2[j - 1]) ? 1 : -1);
-            int tmp = cur_column[i - 1].score - 1;
+      Direction bt;
+      int score;
+      for (row = 1; row < table.length; ++row) {
+         for (column = 1; column < table[0].length; ++column) {
+            // look diagonal
+            bt = Direction.DIAG;
+            score = table[row - 1][column - 1].score + ((s1[row - 1] == s2[column - 1]) ? 1 : -1);
+            // look up
+            int tmp = table[row - 1][column].score - 1;
             if (tmp > score) {
                bt = Direction.UP;
                score = tmp;
             }
-            tmp = prev_column[i].score - 1;
+            // look left
+            tmp = table[row][column - 1].score - 1;
             if (tmp > score) {
                bt = Direction.LEFT;
                score = tmp;
             }
-            cur_column[i].score = score;
-            cur_column[i].backtrack = bt;
+            table[row][column].score = score;
+            table[row][column].backtrack = bt;
          }
-         prev_column = cur_column;
       }
 
       // find position with highest score in last column or last row
-      int best = -1, best_j = -1;
-      for (j = 0; j <= n; ++j) {
-         if (columns[j][m].score >= best) {
-            best_j = j;
-            best = columns[j][m].score;
+      Entry bestEntry = table[m][1];
+      int bestColumn = 1, bestRow = m;
+      for (column = 2; column < table[m].length; ++column) { // start by 2 cause bestEntry is
+                                                             // already set to table[m][1]
+         if (table[m][column].score >= bestEntry.score) {
+            bestColumn = column;
+            bestEntry = table[m][column];
          }
       }
-      assert best_j != -1;
-      int best_i = -1;
-      best = -1;
-      Entry[] last_column = columns[n];
-      for (i = 0; i <= m; ++i) {
-         if (last_column[i].score >= best) {
-            best_i = i;
-            best = last_column[i].score;
-         }
-      }
-      assert best_i != -1;
-      // last_row = [ (columns[j][m], j) for j in xrange(n+1) ]
-      // row_best, best_j = max(last_row)
 
-      if (columns[best_j][m].score > columns[n][best_i].score) {
-         best_i = m;
-         best = columns[best_j][m].score;
-      } else {
-         best_j = n;
-         best = columns[n][best_i].score;
+      for (row = 1; row < table.length - 1; ++row) { // go to table.length-2 cause table[m][n] was
+                                                     // checked in last loop
+         if (table[row][n].score >= bestEntry.score) {
+            bestRow = row;
+            bestColumn = n;
+            bestEntry = table[row][n];
+         }
       }
 
       // now track back
-      byte[] alignment1 = new byte[m + n + 4];
-      byte[] alignment2 = new byte[m + n + 4];
+      byte[] alignment1 = new byte[m + n];
+      byte[] alignment2 = new byte[m + n];
 
       int p1 = 0;
       int p2 = 0;
 
-      i = m;
-      j = n;
+      row = m;
+      column = n;
+
+      assert bestRow == table.length - 1 || bestColumn == table[0].length - 1;
 
       // first, walk from the lower right corner to the
       // position where we found the maximum score
-      if (i == best_i) { // we are in the last row
-         while (j > best_j) {
+      if (table.length - 1 == bestRow) { // we are in the last row
+         while (column > bestColumn) {
             alignment1[p1++] = GAP;
-            alignment2[p2++] = s2[--j];
+            alignment2[p2++] = s2[--column];
          }
       } else { // we are in the last column
-         while (i > best_i) {
-            alignment1[p1++] = s1[--i];
+         while (row > bestRow) {
+            alignment1[p1++] = s1[--row];
             alignment2[p2++] = GAP;
          }
       }
@@ -424,20 +412,20 @@ public class Aligner {
       // now, the actual backtracking.
       // we build reverse sequences while backtracking and
       // reverse them afterwards.
-      while (i > 0 && j > 0) { // columns[j][i] > 0
-         // int score = columns[j][i].score;
-         Direction direction = columns[j][i].backtrack;
+      Direction direction;
+      while (row > 0 && column > 0) {
+         direction = table[row][column].backtrack;
          if (direction == Direction.DIAG) {
-            if (s1[--i] != s2[--j])
+            if (s1[--row] != s2[--column])
                errors++;
-            alignment1[p1++] = s1[i];
-            alignment2[p2++] = s2[j];
+            alignment1[p1++] = s1[row];
+            alignment2[p2++] = s2[column];
          } else if (direction == Direction.LEFT) {
             errors++;
             alignment1[p1++] = GAP;
-            alignment2[p2++] = s2[--j];
+            alignment2[p2++] = s2[--column];
          } else if (direction == Direction.UP) {
-            alignment1[p1++] = s1[--i];
+            alignment1[p1++] = s1[--row];
             alignment1[p2++] = GAP;
             errors++;
          }
@@ -446,39 +434,65 @@ public class Aligner {
       // compute the length of the actual alignment (ignoring ends)
       int length = p1 - rlen;
 
-      int begin = i;
-      if (j > i)
-         begin = j;
-      // continue from where we are to the upper left corner
-      /*
-      while (i > 0 && j > 0) {
-         *p1++ = s1[--i];
-         *p2++ = s2[--j];
-      }
-      */
-      while (j > 0) {
+      int begin = row; //TODO maybe -1?
+      if (column > row)
+         begin = column;
+
+      while (column > 0) {
          alignment1[p1++] = GAP;
-         alignment2[p2++] = s2[--j];
+         alignment2[p2++] = s2[--column];
       }
-      while (i > 0) {
-         alignment1[p1++] = s1[--i];
+      while (row > 0) {
+         alignment1[p1++] = s1[--row];
          alignment2[p2++] = GAP;
       }
-      assert i == 0 && j == 0;
-
-      // print "best score:", columns[best_j][best_i]
-      // print "length:", length
-      // print "errors:", errors
-      assert columns[best_j][best_i].score == length - 2 * errors;
+      assert row == 0 && column == 0;
+      assert table[bestColumn][bestRow].score == length - 2 * errors;
 
       // reverse result
       ArrayUtils.reverseArray(alignment1, p1);
       ArrayUtils.reverseArray(alignment2, p2);
-      // *p1 = '\0';
-      // *p2 = '\0';
+      
+      //cut unused fields in alignments
+      alignment1 = Arrays.copyOf(alignment1, p1);
+      alignment2 = Arrays.copyOf(alignment2, p2);
 
-      // PyObject* o = Py_BuildValue("ssiii", alignment1, alignment2, begin, length, errors);
-      // return o;
+      return new SemiglobalAlignmentResult(alignment1, alignment2, begin, length, errors);
+   }
+   
+   public static class SemiglobalAlignmentResult {
+      private final byte[] sequence1, sequence2;
+
+      private final int begin, length, errors;
+
+      public SemiglobalAlignmentResult(byte[] sequence1, byte[] sequence2, int begin, int length,
+            int errors) {
+         this.sequence1 = sequence1;
+         this.sequence2 = sequence2;
+         this.begin = begin;
+         this.length = length;
+         this.errors = errors;
+      }
+
+      public byte[] getSequence1() {
+         return sequence1;
+      }
+
+      public byte[] getSequence2() {
+         return sequence2;
+      }
+
+      public int getBegin() {
+         return begin;
+      }
+
+      public int getLength() {
+         return length;
+      }
+
+      public int getErrors() {
+         return errors;
+      }
    }
 
    // TODO perhaps remove static
