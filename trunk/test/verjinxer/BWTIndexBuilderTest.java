2fb8045a0ebff96bbc94bffedfc8574a3da537a7
markus.kemmerling@uni-dortmund.de <markus.kemmerling@uni-dortmund.de@94ea493a-7c3b-0410-831d-612bdd61bfce>
2009-09-11 15:57:52 +0000

    Reference implementation for building a bwt index for test purpose.

--- trunk/test/verjinxer/BWTIndexBuilderTest.java
+++ trunk/test/verjinxer/BWTIndexBuilderTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import verjinxer.sequenceanalysis.Alphabet;
@@ -14,7 +15,7 @@ import verjinxer.sequenceanalysis.Sequences;
 import verjinxer.sequenceanalysis.SuffixXorDLL;
 
 public class BWTIndexBuilderTest {
-
+   
    @Test
    public void testBuild01() {
       byte[] s = {0,0,3,0,2,0,0,3,0,2,4,1}; //eenemeenemuh
@@ -113,4 +114,98 @@ public class BWTIndexBuilderTest {
       
       assertTrue(reference.equals(index));
    }
+   
+   @Test
+   public void testBuild03() {
+      byte[] b = {3,3,1,1,3,0,1,3,3,1,0,2,2,2,1,1,0,3,0,0,0,2,1,1,3};
+      bwt(b);
+   }
+   
+   
+   public static void bwt(final byte[] seq) {
+      StringBuilder sb = new StringBuilder(seq.length);
+      for(byte b: seq) {
+         sb.append(b);
+      }
+      String org = sb.toString();
+      
+      String[] bwt_matrix = new String[seq.length];
+      
+      bwt_matrix[0] = org;
+      for (int i = 1; i < bwt_matrix.length; i++) {
+         bwt_matrix[i] = bwt_matrix[i-1].substring(1) + bwt_matrix[i-1].charAt(0);
+      }
+      
+      //print matrix
+      System.out.println("Matrix");
+      for(String s: bwt_matrix) {
+         System.out.println(s);
+      }
+      
+      Arrays.sort(bwt_matrix);
+      //print matrix
+      System.out.println("-----------------------------------------------------------------------------------------");
+      System.out.println("sorted Matrix");
+      for(String s: bwt_matrix) {
+         System.out.println(s);
+      }
+      
+      byte[] e = new byte[bwt_matrix.length];
+      byte[] l = new byte[bwt_matrix.length];
+      for (int i = 0; i < bwt_matrix.length; i++) {
+         e[i] = Byte.parseByte(bwt_matrix[i].charAt(0)+"");
+         l[i] = Byte.parseByte(bwt_matrix[i].charAt(bwt_matrix[i].length()-1)+"");
+      }
+      
+      int[] counter = new int [256];
+      for (byte b: e) {
+         counter[b+128]++;
+      }
+      
+      int[] c = new int[256];
+      for(int i = 1; i < c.length; i++) {
+         c[i] = c[i-1] + counter[i-1];
+      }
+      
+      counter = Arrays.copyOf(c, c.length);
+      
+      int[] el = new int[e.length];
+      for(int i = 0; i < l.length; i++) {
+         final byte b = l[i];
+         final int bi = b + 128;
+         el[counter[bi]] = i;
+         counter[bi]++;
+      }
+      
+      //print c
+      System.out.println("-----------------------------------------------------------------------------------------");
+      System.out.println("c");
+      for(int i = 0; i < c.length; i++) {
+         System.out.printf("%s: %s%n", i-128, c[i]);
+      }
+      
+      //print e
+      System.out.println("-----------------------------------------------------------------------------------------");
+      System.out.println("e");
+      for(int i = 0; i < e.length; i++) {
+         System.out.printf("%s: %s%n", i, e[i]);
+      }
+      
+      //print l
+      System.out.println("-----------------------------------------------------------------------------------------");
+      System.out.println("l");
+      for(int i = 0; i < e.length; i++) {
+         System.out.printf("%s: %s%n", i, l[i]);
+      }
+      
+      //print el
+      System.out.println("-----------------------------------------------------------------------------------------");
+      System.out.println("el");
+      for(int i = 0; i < e.length; i++) {
+         System.out.printf("%s: %s%n", i, el[i]);
+      }
+      
+      
+   }
 }
+ 
